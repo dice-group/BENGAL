84b93a57b7c13f805eab3c432ea37404b357e54d
mroeder <roeder@informatik.uni-leipzig.de>
2016-02-09 12:37:46 +0100

    Added an example of the creation of a single document and a named entity. Implemented the general workflow of the generateCorpus method. Added SLF4J, log.properties and JUnit.

--- src/main/java/org/aksw/simba/bengal/controller/BengalController.java
+++ src/main/java/org/aksw/simba/bengal/controller/BengalController.java
@@ -5,17 +5,76 @@
  */
 package org.aksw.simba.bengal.controller;
 
+import java.io.FileOutputStream;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+
+import org.aksw.gerbil.io.nif.NIFWriter;
+import org.aksw.gerbil.io.nif.impl.TurtleNIFWriter;
+import org.aksw.gerbil.transfer.nif.Document;
+import org.aksw.simba.bengal.paraphrasing.Paraphraser;
+import org.aksw.simba.bengal.selector.TripleSelector;
+import org.aksw.simba.bengal.verbalizer.Verbalizer;
 import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.Statement;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  *
  * @author ngonga
+ * @author Michael R&ouml;der (roeder@informatik.uni-leipzig.de)
  */
 public class BengalController {
-    
-    public void generateCorpus (Map<String, String> parameters, Model data, String corpusName)
-    {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(BengalController.class);
+
+    public void generateCorpus(Map<String, String> parameters, Model data, String corpusName) {
+        // TODO instantiate components;
+        TripleSelector tripleSelector = null;
+        Verbalizer verbalizer = null;
+        Paraphraser paraphraser = null;
+
+        // TODO get the number of documents from the parameters
+        int numberOfDocuments = 0;
+
+        List<Statement> triples;
+        Document document;
+        List<Document> documents = new ArrayList<Document>();
+        while (documents.size() < numberOfDocuments) {
+            // TODO select triples
+            triples = tripleSelector.getNextStatements();
+            // TODO create document
+            document = verbalizer.generateDocument(triples);
+            // TODO paraphrase document
+            document = paraphraser.getParaphrase(document);
+            // If the generation and paraphrasing were successful
+            if (document != null) {
+                documents.add(document);
+            }
+        }
+
+        // TODO generate file name and path from corpus name
+        String filePath = null;
+        // write the document
+        NIFWriter writer = new TurtleNIFWriter();
+        FileOutputStream fout = null;
+        try {
+            fout = new FileOutputStream(filePath);
+            writer.writeNIF(documents, fout);
+        } catch (Exception e) {
+            LOGGER.error("Error while writing the documents to file. Aborting.", e);
+        } finally {
+            if (fout != null) {
+                try {
+                    fout.close();
+                } catch (Exception e) {
+                    // nothing to do
+                }
+            }
+        }
+
         return;
     }
 }
--- src/main/java/org/aksw/simba/bengal/verbalizer/SemWeb2NLVerbalizer.java
+++ src/main/java/org/aksw/simba/bengal/verbalizer/SemWeb2NLVerbalizer.java
@@ -7,16 +7,25 @@ package org.aksw.simba.bengal.verbalizer;
 
 import java.util.List;
 import org.aksw.gerbil.transfer.nif.Document;
+import org.aksw.gerbil.transfer.nif.data.DocumentImpl;
+import org.aksw.gerbil.transfer.nif.data.NamedEntity;
 import org.apache.jena.rdf.model.Statement;
 
 /**
  * A deterministic verbalizer which relies on the SemWeb2NL project.
+ * 
  * @author ngonga
  * @author roeder
  */
 public class SemWeb2NLVerbalizer implements Verbalizer {
 
     public Document generateDocument(List<Statement> triples) {
-        return null;
-    }    
+
+        // This is a simple example that shows how a document can be created
+        Document document = new DocumentImpl("PLEASE GIVE ME A URI!!!");
+        int startPosition = 0;
+        int length = 10;
+        document.addMarking(new NamedEntity(startPosition, length, "ENTITY URI"));
+        return document;
+    }
 }
