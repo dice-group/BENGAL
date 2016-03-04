ae4205a7fea01edeee681a8cc625450a1183b7c9
mroeder <roeder@informatik.uni-leipzig.de>
2016-03-04 18:30:26 +0100

    Added code snippets for the ne position update.

--- src/main/java/org/aksw/simba/bengal/paraphrasing/Paraphraser.java
+++ src/main/java/org/aksw/simba/bengal/paraphrasing/Paraphraser.java
@@ -6,7 +6,13 @@
 package org.aksw.simba.bengal.paraphrasing;
 
 
+import java.util.List;
+
 import org.aksw.gerbil.transfer.nif.Document;
+import org.aksw.gerbil.transfer.nif.Span;
+import org.aksw.gerbil.transfer.nif.data.DocumentImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.memetix.mst.language.Language;
 import com.memetix.mst.translate.Translate;
@@ -18,6 +24,9 @@ import com.memetix.mst.translate.Translate;
  * @author ngonga
  */
 public class Paraphraser {
+    
+    private static final Logger LOGGER = LoggerFactory.getLogger(Paraphraser.class);
+    
     public Document getParaphrase (Document doc) throws Exception{
         Translate.setClientId("Moussallem");
         Translate.setClientSecret("Sq2m3JmTIcdoA5diD2Mw4Clh6sNpoRp5e4uxcXpMwT8=");
@@ -26,8 +35,20 @@ public class Paraphraser {
         
         String translatedText = Translate.execute(text, Language.ENGLISH, Language.ITALIAN);
         String paraphrases = Translate.execute(translatedText, Language.ITALIAN, Language.ENGLISH);
- 
+
         doc.setText(paraphrases);
+        Document newDoc = new DocumentImpl(paraphrases, doc.getDocumentURI(), doc.getMarkings());
+        // Update named entities
+        List<Span> spans = newDoc.getMarkings(Span.class);
+        int pos;
+        for (Span span : spans) {
+            // TODO search the position in the old text
+            // TODO Update the position in the new text
+            // TODO if position search fails, return the original document and log a message like:
+            LOGGER.warn("Paraphrasing changed one of the entities. Returning document without paraphrasing.");
+            return doc;
+        }
+ 
         return doc;
     
     }
